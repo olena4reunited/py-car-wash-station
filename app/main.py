f0f4101dd91996a4488ff4cbc2e2766cabef31da
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-07-25 11:41:58 +0000

    Fix-parameters-placement

--- app/main.py
+++ app/main.py
@@ -1,22 +1,31 @@
 def limitation(
-        value: float, min_value: float, max_value: float) -> float:
+        value: float,
+        min_value: float,
+        max_value: float
+) -> float:
     return max(min(value, max_value), min_value)
 
 
 class Car:
     def __init__(
-            self, comfort_class: int, clean_mark: int, brand: str) -> None:
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = limitation(comfort_class, 1, 7)
         self.clean_mark = limitation(clean_mark, 1, 10)
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = limitation(
             distance_from_city_center, 1.0, 10.0)
         self.clean_power = limitation(clean_power, 1, 10)
